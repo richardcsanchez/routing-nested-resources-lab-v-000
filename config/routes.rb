e0ab78e8837d33f3f67176cd4cb7d9549ea0792f
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-17 19:43:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists do
-    resources :songs
+  resources :artists, only: [:show] do
+    resources :songs, only: [:show, :index]
   end
-  resources :songs
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  root 'songs#index'
 end
