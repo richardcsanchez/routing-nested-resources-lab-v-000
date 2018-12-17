15fafc9a523c73ab057f326d1818ef9e6fd79ce9
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-17 20:20:42 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,6 +17,7 @@ class SongsController < ApplicationController
   if params[:artist_id]
     @artist = Artist.find_by(id: params[:artist_id])
     if @artist.nil?
+      flash[:alert] = "Artist not found."
       redirect_to artists_path
     else
       @song = @artist.songs.find_by(id: params[:id])
